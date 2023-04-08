40d4a4fe4da593b39e9cee3e0b279b4e5b5f2306
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-04-08 10:20:50 +0500

    material

--- routes/material.route.js
+++ routes/material.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const MaterialController = require('../controller/material.controller');
 const { upload } = require('../helpers/uloadImage');
-// const authAdmin = require("../middleware/adminAuth");
-// const auth = require("../middleware/auth.middleware");
+const authAdmin = require("../middleware/adminAuth");
+const auth = require("../middleware/auth.middleware");
 
 /* GET home page. */
-router.post('/create', upload.single('file'), MaterialController.create);
+router.post('/create', auth, authAdmin, upload.single('file'), MaterialController.create);
 module.exports = router;
