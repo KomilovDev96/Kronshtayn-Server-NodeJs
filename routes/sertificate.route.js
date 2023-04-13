3430909f76006fdbb8cfa5993481261ff1199a98
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-04-13 19:14:53 +0500

    auth

--- routes/sertificate.route.js
+++ routes/sertificate.route.js
@@ -6,9 +6,9 @@ const authAdmin = require("../middleware/adminAuth");
 const auth = require("../middleware/auth.middleware");
 
 /* GET home page. */
-router.post('/create',  upload.single('file'), SertificateController.create);
-router.put('/update/:id',  upload.single('file'), SertificateController.update)
-router.delete('/delete/:id',  SertificateController.delete)
+router.post('/create', auth, authAdmin, upload.single('file'), SertificateController.create);
+router.put('/update/:id', auth, authAdmin, upload.single('file'), SertificateController.update)
+router.delete('/delete/:id', auth, authAdmin, SertificateController.delete)
 router.get('/read/:id', SertificateController.read);
 router.get('/getall', SertificateController.getAll);
 module.exports = router;
