2a5bbcc8bcce41a0fc91293edaa4dc0edd82f18b
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-04-13 23:45:28 +0500

    fix

--- routes/news.route.js
+++ routes/news.route.js
@@ -6,9 +6,9 @@ const authAdmin = require("../middleware/adminAuth");
 const auth = require("../middleware/auth.middleware");
 
 /* GET home page. */
-router.post('/create', upload.single('file'), NewsController.create);
+router.post('/create', auth, authAdmin, upload.single('file'), NewsController.create);
 router.get('/read/:id', NewsController.read);
 router.get('/getall', NewsController.getAll);
-router.delete('/delete/:id', NewsController.delete)
-router.put('/update/:id', upload.single('file'), NewsController.update)
+router.delete('/delete/:id', auth, authAdmin, NewsController.delete)
+router.put('/update/:id', auth, authAdmin, upload.single('file'), NewsController.update)
 module.exports = router;
